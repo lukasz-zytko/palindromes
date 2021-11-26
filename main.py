22dae50c431150a5edae26871f100c2555793d5b
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-11-26 22:07:08 +0100

    Add function for palndrome check

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+def czy_palindrom(wyraz):
+    back = str()
+    for i in range(len(wyraz)-1, -1, -1):
+        back += wyraz[i]
+    if wyraz == back:
+        return True
+    else:
+        return False
 
+check = czy_palindrom("nie kajak")
+print(check)
