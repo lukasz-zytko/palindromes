94fd23ea6eb775545ac4b2decdbb7210153b0c0a
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-11-26 22:27:39 +0100

    Add capital sensitive

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def czy_palindrom(wyraz):
+    wyr = wyraz.lower()
     back = str()
-    for i in range(len(wyraz)-1, -1, -1):
-        back += wyraz[i]
-    if wyraz == back:
-        return True
-    else:
-        return False
+    for i in range(len(wyr)-1, -1, -1):
+        back += wyr[i]
+    return True if wyr == back else False
+
+print(czy_palindrom("MAm"))
