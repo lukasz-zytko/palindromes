69cc1078bb0cae549fed75d8f05aacea932ccf37
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-11-26 22:38:36 +0100

    Add documentation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def czy_palindrom(wyraz):
+    """
+        Checks whether the given argument is a palindrome
+        Arguments:
+        string
+    """
     wyr = wyraz.lower()
     back = str()
     for i in range(len(wyr)-1, -1, -1):
